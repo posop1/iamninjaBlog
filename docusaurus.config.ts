731af88ef8158a178ee9ba84ca82dcbcb6dd2fa5
Artur Yakhin <artur.yakhin52@gmail.com>
2025-01-31 02:37:15 +0500

    first commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,137 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer').themes.nightOwlLight;
+const darkCodeTheme = require('prism-react-renderer').themes.nightOwl;
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Iamninja Blog',
+  favicon: 'img/favicon.webp',
+  // tagline: 'Всякое гиковское',
+  url: 'https://iamninja.ru',
+  baseUrl: '/',
+
+  onBrokenLinks: 'throw',
+  // onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Iamninja', // Usually your GitHub org/user name.
+  projectName: 'iamninjaBlog', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  // plugins: [
+  //   [
+  //     'docusaurus-plugin-yandex-metrica',
+  //     {
+  //       counterID: '49559035',
+  //     },
+  //   ],
+  // ],
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      {
+        docs: {
+          routeBasePath: '/docs',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/AMD-NICK/docusaurustest/tree/main/',
+          showLastUpdateTime: true,
+        },
+
+        blog: {
+          routeBasePath: '/',
+          showReadingTime: true,
+          blogSidebarCount: 'ALL',
+          blogSidebarTitle: 'Другие посты',
+          // Remove this to remove the "edit this page" links.
+          // editUrl: 'https://github.com/AMD-NICK/docusaurustest/tree/main/',
+        },
+
+        theme: {
+          customCss: [
+            // not my styles. Taken from here:
+            // https://github.com/vendure-ecommerce/vendure/blob/cc4826dfb7c1a2f4e6ed8daa13eb017090d8bd9a/docs/src/css/custom.css
+            require.resolve('./src/css/custom.css'),
+            require.resolve('./src/css/layout.css'),
+            require.resolve('./src/css/overrides.css'),
+            // require.resolve('./src/css/code-blocks.css'),
+          ],
+        },
+
+        gtag: {
+          trackingID: 'G-SXVYQX65GD',
+          anonymizeIP: true,
+        },
+      },
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    {
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
+
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Лого',
+          src: 'img/logo.png', // todo webp
+        },
+        items: [
+          {
+            type: 'dropdown',
+            label: '✍️ Блог',
+            position: 'left',
+            items: [
+              { label: 'Главная', to: '/' },
+              { label: 'Все по годам', to: 'archive' },
+            ],
+          },
+          {
+            type: 'dropdown',
+            label: '📦 Ресурсы',
+            position: 'left',
+            items: [
+              { label: '🧠 База знаний', type: 'doc', docId: 'intro' },
+              { label: '👤 Обо мне', to: 'about' },
+              { label: '✈️ TG Боты', to: 'docs/telegram/bots' },
+            ],
+          },
+          {
+            href: 'https://github.com/AMD-NICK/blog.amd-nick.me',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['lua', 'bash', 'json'],
+      },
+      algolia: {
+        appId: '03RIRS86OT',
+        apiKey: 'a74f58efb237f210291f299f0c7e8769',
+        indexName: 'amd-nick',
+      },
+    },
+};
+
+module.exports = config;
---
+++ src/theme/BlogPostItem.tsx
@@ -0,0 +1,41 @@
+// based on:
+// https://github.com/IceWhaleTech/ZimaDocs/blob/7ddfa8360dcab96126ca49ce6c2fa87394f20b63/src/theme/DocPaginator/index.js
+// deleted comments front matter option and i18n
+
+import React from 'react';
+import BlogPostItem from '@theme-original/BlogPostItem';
+import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
+import { useColorMode } from '@docusaurus/theme-common';
+import Giscus from '@giscus/react';
+
+export default function BlogPostItemWrapper(props) {
+  const { colorMode } = useColorMode();
+  const { isBlogPostPage } = useBlogPost();
+  if (!isBlogPostPage) {
+    return <BlogPostItem {...props} />;
+  }
+
+  return (
+    <>
+      <BlogPostItem {...props} />
+      {
+        <div className="docusaurus-blog-comments">
+          <Giscus
+            id="blog_comments"
+            repo="AMD-NICK/blog.amd-nick.me"
+            repoId="R_kgDOHVBfKA"
+            category="Blog comments"
+            categoryId="DIC_kwDOHVBfKM4CRnM9"
+            mapping="pathname"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme={colorMode === 'dark' ? 'transparent_dark' : 'light'}
+            lang={'ru'}
+            loading="lazy"
+          />
+        </div>
+      }
+    </>
+  );
+}
